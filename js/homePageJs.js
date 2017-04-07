f630b611538064bed440b475af5bbaa4501b55b5
muzichujiao <yaoluohuayu@qq.com>
2017-04-07 20:22:37 +0800

    second

---
+++ js/homePageJs.js
@@ -0,0 +1,19 @@
+ $(document).ready(function(){
+    	$('#showimg').mouseover(function(){
+    		 $('#imgg').animate({
+                    width:"auto",
+                    height:"100px",
+              	 	marginLeft:"-5px",
+                    marginTop:"-5px"
+                },400);  
+    		});
+    	$('#showimg').mouseout(function(){
+    		$('#imgg').animate({
+    			width:"auto",
+    			height:"95px",
+    			marginLeft:"0px",
+                marginTop:"0px"
+    		},400);
+    	});         
+    });
+ 
